10e014edbf7dbf38a7a15a92b081667290711140
xkr-xx <xkrgidwjd916@pusan.ac.kr>
2022-10-27 00:00:22 +0900

    수정..10.27 탁향정

---
+++ js/index.js
@@ -0,0 +1,34 @@
+/* menu on off */
+$('#menu').click(function(){
+  $('#menu').fadeOut();
+  $('#menu-cont').fadeIn();
+})
+$('#menu-close').click(function(){
+  $('#menu-cont').fadeOut();
+  $('#menu').fadeIn();
+})
+
+/* modal on off */
+body =  document.querySelector('body');
+$('.inter .btn-modal__open').click(function(){
+  console.log('d')
+  $('.modal-cont').fadeIn();
+  body.style.overflow = 'hidden';
+})
+$('.modal-cont.designer-inter').click(function(){
+  $('.modal-cont').fadeOut();
+  body.style.overflow = 'auto';
+})
+
+
+/* PC 모바일 구분 */
+// var filter = "win16|win32|win64|mac|macintel";
+// if ( navigator.platform ) {
+// 	if ( filter.indexOf( navigator.platform.toLowerCase() ) < 0 ) {
+// 		//mobile
+// 		alert('본 페이지는 PC에 최적화된 웹페이지입니다.')
+// 	} // else {
+// 	// 	//pc
+// 	// 	$('.mobile').css('display', 'none');
+//   // }
+// }
